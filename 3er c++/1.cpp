3f1059243681bad37a72ae14b30417d11b018736
Monter Valadez Diego Alexander 2BMPG <169079054+DiegoMonter08@users.noreply.github.com>
2024-06-03 10:49:44 -0600

    Update 1.cpp

--- 3er c++/1.cpp
+++ 3er c++/1.cpp
@@ -1,19 +1,24 @@
 #include <iostream>
 using namespace std;
 int main() {
+    int total = 0;
+    for (int i = 0; i < 3; ++i) {
     int unidades, precio, subtotal;
 
     cout << "Ingrese la cantidad de unidades: ";
     cin >> unidades;
-    cout << "Cantidad de unidades: " << unidades << std::endl;
+    cout << "Cantidad de unidades: " << unidades <<endl;
 
     cout << "Ingrese el precio: ";
     cin >> precio;
-    cout << "Precio: " << precio << std::endl;
+    cout << "Precio: " << precio <<endl;
 
     subtotal = unidades * precio;
-    cout << "Subtotal: " << subtotal << std::endl;
+    cout << "Subtotal: " << subtotal <<endl;
 
+    total += subtotal;
+    }
+    cout<< "El total de los tres subtotales es: " <<total << endl;
     return 0;
 }
 
